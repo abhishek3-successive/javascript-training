e75f3b40592465469d5cb2b82d740d447943f840
Abhishek Gupta <abhishek.gupta1@abhishekguptass-454.successivetech.com>
2025-06-05 09:51:22 +0530

    updated-code

--- Day 3/fibanaaci-Q9.js
+++ Day 3/fibanaaci-Q9.js
@@ -1,19 +1,22 @@
 // 9. Write a program to print fibonaaci series
-// const fibonaaci = function(n){
-//     if(n<=1){
-//         return n;
-//     }
-//     return fibonaaci(n-1)+fibonaaci(n-2)
-// }
-// fibonaaci(10);
-
-var a = 0;
-var b = 1;
-console.log(a);
-console.log(b);
-for(var i= 3; i<20; i++){
-    var c = a+b;
-    console.log(c);
-    a=b;
-    b=c;
+const fibonaaci = function(n){
+    if(n<=1){
+        return n;
+    }
+    return fibonaaci(n-1)+fibonaaci(n-2)
 }
+let n = 10
+for(let i = 0; i<=n ; i++){
+console.log(fibonaaci(i));
+}
+
+// var a = 0;
+// var b = 1;
+// console.log(a);
+// console.log(b);
+// for(var i= 3; i<20; i++){
+//     var c = a+b;
+//     console.log(c);
+//     a=b;
+//     b=c;
+// }
