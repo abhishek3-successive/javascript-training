ca9c4c8d4301cdc9d4c06458308c4e912167c698
Abhishek Gupta <abhishek.gupta1@abhishekguptass-454.successivetech.com>
2025-06-06 13:43:07 +0530

    new-way

--- Day 3/print-prime_Q6.js
+++ Day 3/print-prime_Q6.js
@@ -1,21 +1,15 @@
 // 6. Write a program to display prime numbers from 1 to 50
-function primeno(){
-    for(let i=2;i<=50;i++)
-    {
-        let isP=true;
-        for(let j=2;j<=Math.sqrt(i);j++)
-        {
-            if(i%j===0)
-            {
-                isP=false;
-            }
-        }
-
-        if(isP)
-        {
-            console.log(i);
+for(let i = 2; i<=50; i++){
+    let isP = true;
+    for(let j = 2; j<i; j++){
+        if(i%j==0){
+            isp= false;
+            break
         }
     }
-}
 
-primeno();
+    if(isP){
+        console.log(i);
+        
+    }
+}
