17ddff8d4778712dc611e97dc16416dccbc1f124
abhishek3-successive <abhishek.gupta3@successive.tech>
2025-06-09 18:20:12 +0530

    updated-changes

--- Day 2/input-output_Q2.js
+++ Day 2/input-output_Q2.js
@@ -1,9 +1,13 @@
 // 2. Write a function to take input(age, name, phone no.) from user and display it
-const age = prompt("Enter your age")
-const nam = prompt("Enter your name")
-const phone = prompt("Enter your phone")
+var prompt = require('prompt-sync')(); // for taking values
+const age = Number(prompt("Enter your age " + ":" )) // age it should be num 
+const nam = prompt("Enter your name "+ ":" ) // it should be character
+const phone = Number(prompt("Enter your phone "+ ":" )) // number  it should be 10 
 
-console.log(age)
-console.log(nam)
-console.log(phone)
-
+console.log("age "+":"+age)
+if(/^[A-Za-z]+$/.test(nam)){
+    console.log("Name " + nam)
+}
+if(!/^\d{10}$/.test(phone)){
+console.log("Phone no "+ ": "+phone)
+}
