5d06414761dcce08d59aa6049e9f18ee1511e084
Abhishek Gupta <abhishek.gupta1@abhishekguptass-454.successivetech.com>
2025-06-11 14:53:26 +0530

    removing-files

--- Day 2/squre_Q4.js
+++
@@ -1,5 +0,0 @@
-// 4. Write a function expression that takes in a number and returns its square.
-const sq = (num)=>{
-    return num*num
-}
-console.log(sq(8));
--- Day 4/remove-duplicate_Q8.js
+++
@@ -1,9 +0,0 @@
-// 8. Write a program to remove dupliacte elements from an array
-var arr = [3,4,3,4,3]
-var res = []
-for(let i = 0 ; i<arr.length; i++){
-   if(!res.includes(arr[i])){
-    res.push(arr[i])
-   }
-}
-console.log(res)
