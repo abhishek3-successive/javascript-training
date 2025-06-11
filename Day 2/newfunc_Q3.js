ea8698f636441c5d2436aa1dc5ad85519eb56650
Abhishek Gupta <abhishek.gupta1@abhishekguptass-454.successivetech.com>
2025-06-11 16:35:06 +0530

    fix/branch

--- Day 2/newfunc_Q3.js
+++
@@ -1,9 +0,0 @@
-// 3. Write a function expression that takes in another function as an argument
-const hello= (name, call)=>{
-    console.log("hello", name)
-    call()
-}
-const riya = ()=>{
-     console.log("riya")
-}
-hello("john", riya)
