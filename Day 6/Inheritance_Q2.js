7a5445f1f4cb0fb7730ad30aec5483a0e5a0f3ad
Abhishek Gupta <abhishek.gupta1@abhishekguptass-454.successivetech.com>
2025-06-13 04:24:38 +0530

    updated

--- Day 6/Inheritance_Q2.js
+++ Day 6/Inheritance_Q2.js
@@ -1,38 +1,42 @@
-//  Create a class Shape with properties width and height and methods getArea(). Create two classes Rectangle and Triangle that inherit from the Shape class and implement the getArea() method for their respective shapes.
 class Shape {
-    constructor( width , height){
-        this.width = width
-        this.height = height
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
     }
-    getArea(){
-        console.log(`Area of shape : ${this.height*this.width}`)
+
+    getArea() {
+        console.log(`Area of shape: ${this.height * this.width}`);
     }
 }
 
-class Rectangle extends Shape{
-    constructor( width , height){
-
-        super()
-        this.width = width
-        this.height = height
+class Rectangle extends Shape {
+    constructor(rectWidth, rectHeight) {
+        super(rectWidth, rectHeight); // Using super to initialize the base class
+        this.rectWidth = rectWidth;  // Rename width to rectWidth for Rectangle
+        this.rectHeight = rectHeight; // Rename height to rectHeight for Rectangle
     }
-    getArea(){
-        console.log(`Area of Rectangle : ${this.width * this.height}`)
+
+    getArea() {
+        console.log(`Area of Rectangle: ${this.rectWidth * this.rectHeight}`);
     }
 }
 
-class Triangle extends Shape{
-    constructor(width , height){
-        super(width , height)
+class Triangle extends Shape {
+    constructor(triWidth, triHeight) {
+        super(triWidth, triHeight); // Using super to initialize the base class
+        this.triWidth = triWidth;   // Rename width to triWidth for Triangle
+        this.triHeight = triHeight; // Rename height to triHeight for Triangle
     }
-    getArea(){
-        console.log(`Area of Triangle : ${1/2*this.height*this.width}`)
+
+    getArea() {
+        console.log(`Area of Triangle: ${0.5 * this.triWidth * this.triHeight}`);
     }
 }
 
-let A = new Shape(15,60)
-let B = new Triangle(15,60)
-let C = new Rectangle(15,60)
-A.getArea()
-B.getArea()
-C.getArea()
+let A = new Shape(15, 60);
+let B = new Triangle(15, 60);
+let C = new Rectangle(15, 60);
+
+A.getArea();
+B.getArea();
+C.getArea();
--- Day 6/Person-Student-Teacher_Q3..js
+++ Day 6/Person-Student-Teacher_Q3..js
@@ -1,7 +1,4 @@
 // 3.1 Create a class Person with properties name, age, gender, and interests. Add a method greeting() that returns a string introducing the person. Also add a method farewell() that returns a string saying goodbye to the person.
-// 3.2 Create a class Student that inherits from the Person class and has a property studies. Override the greeting() method to include information about what the student is studying.
-// 3.3 Create a class Teacher that inherits from the Person class and has a property subjectsTaught. Override the farewell() method to include information about what the teacher teaches.
-
 class person{
     constructor(name , age , gender , interests){
         this.name = name
@@ -17,15 +14,19 @@ class person{
     }
 }
 
+// 3.2 Create a class Student that inherits from the Person class and has a property studies. Override the greeting() method to include information about what the student is studying.
 class Student extends person{
     constructor( name , age , gender , interests,study){
-        super(name , age , gender , interests)
+        super(name , age , gender , interests) // initize the base class
         this.study = study
     }
     greeting(){
         console.log(`Hii my name is ${this.name} and my age is ${this.age} & I have a keen interest in ${this.interests} and i study ${this.study}`)
     }
 }
+
+// 3.3 Create a class Teacher that inherits from the Person class and has a property subjectsTaught. Override the farewell() method to include information about what the teacher teaches.
+
 class Teacher extends person{
     constructor(name , age , gender , interests , subjectsTaught ){
         super(name , age , gender , interests)
@@ -35,6 +36,8 @@ class Teacher extends person{
         console.log(`Teacher taught the subject ${this.subjectsTaught}`)
     }
 }
+// objects
+
 let p1 = new person("Goku",23,"M","Gym with CS")
 let p2 = new Student("Goku",23,"M","Gym with CS","Maths")
 let p3 = new Teacher("Goku",23,"M","Gym with CS","computerScience")
