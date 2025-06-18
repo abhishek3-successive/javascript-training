d90023d292b244c81d2188026ed3bd4310480a16
Abhishek Gupta <abhishek.gupta1@abhishekguptass-454.successivetech.com>
2025-06-18 15:21:27 +0530

    add/.gitignore

--- Day 8 /Q12.js
+++ Day 8 /Q12.js
@@ -4,12 +4,19 @@
 //    console.log(data);
 // });
 
+
+// corrected code 
+
 async function getData() {
-  setTimeout(function () {
-    return [1, 2, 3, 4, 5];
-  }, 1000);
-}          // not understanding
+  {
+    return new Promise((resolve, reject) => {
+      setTimeout(function () {
+        return [1, 2, 3, 4, 5];
+      }, 10000)
+      resolve([1,2,3,4,5]);
+    });
+  }
+}
 getData().then(function (data) {
   console.log(data);
 });
-
